a6dd8e970c81fe5a649881fbc15aff384eae31a4
Alex Meza <mezawebdev@gmail.com>
2021-11-09 01:07:18 -0800

    change

--- src/pages/_middleware.ts
+++ src/pages/_middleware.ts
@@ -3,12 +3,11 @@ import { NextResponse } from 'next/server';
 import Configuration from 'src/app/Game/Configuration';
 import ApiUtils from 'src/app/Game/Utils/ApiUtils';
 
-
 export async function middleware(req: NextRequest, ev: NextFetchEvent) {
-    const config = new Configuration(),
-        token = req.cookies.token;
-
     if (req.url === "/") {
+        const config = new Configuration(),
+            token = req.cookies.token;
+
         if (typeof token === "string") {
             try {
                 await fetch(ApiUtils.url(config.network.routes.auth), {  headers: { Authorization: `Bearer ${ token }` } });
